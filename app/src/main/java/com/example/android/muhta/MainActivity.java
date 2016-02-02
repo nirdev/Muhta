2db1b70669a709f9203c0f829ce67b7597f5a4e9
Adi Reznik <adi@digonapp.com>
2016-02-02 14:43:56 +0200

    contacts work!

--- app/src/main/java/com/example/android/muhta/MainActivity.java
+++ app/src/main/java/com/example/android/muhta/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         ParseUser currentUser = ParseUser.getCurrentUser();
